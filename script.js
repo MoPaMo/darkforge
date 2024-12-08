789f8b189fa094c87d641d16289ca4c3949c4c18
MoPaMo <mopamo@users.noreply.github.com>
2024-12-08 16:09:41 +0100

    Add resize handling for Three.js camera and renderer in script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+//set up scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 console.log('Hello World!');
+
+
+
+//handle resizes
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
